ec4e5277a8d93a232c60946075aab1da2bb6b0c8
Michael Bien <mbien@fh-landshut.de>
2009-09-05 12:30:31 +0200

    cleanup

--- resources/CL/stdint.h
+++ resources/CL/stdint.h
@@ -6,14 +6,14 @@
     // typedef unsigned __int64 uint64_t;
 #else
 
-    typedef signed char       int8_t;
+    typedef signed   char     int8_t;
     typedef unsigned short    int16_t;
-    typedef int               int32_t;
+    typedef          int      int32_t;
     typedef unsigned char     uint8_t;
     typedef unsigned short    uint16_t;
     typedef unsigned int      uint32_t;
 
-    typedef int               intptr_t;
+    typedef          int      intptr_t;
     typedef unsigned int      uintptr_t;
 
     typedef unsigned int      size_t;

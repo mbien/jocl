47ce642729bbc1dc5cee7032d3b3d84cb9b82eca
Michael Bien <mbien@fh-landshut.de>
2010-02-21 00:07:44 +0100

    enabled automatic download of cl_ext.h in build. enabled CL_PLATFORM_NOT_FOUND_KHR exception cause (OpenCL headers have been fixed).

--- src/com/mbien/opencl/CLException.java
+++ src/com/mbien/opencl/CLException.java
@@ -176,11 +176,10 @@ public class CLException extends RuntimeException {
                 return "CL_INVALID_MIP_LEVEL";
 
             case CL_INVALID_GLOBAL_WORK_SIZE:
-                return "CL_INVALID_GLOBAL_WORK_SIZE or CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+                return "CL_INVALID_GLOBAL_WORK_SIZE";
 
-// error-code conflict with CL_INVALID_GLOBAL_WORK_SIZE
-//            case CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR:
-//                return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+            case CL_PLATFORM_NOT_FOUND_KHR:
+                return "CL_PLATFORM_NOT_FOUND_KHR";
 
             default:
                 return "unknown cause: code" + error;

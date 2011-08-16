4c8220faf4e0be3cd33e68e82412d103ab6457e9
Michael Bien <mbien@fh-landshut.de>
2011-08-16 18:33:43 +0200

    removed old scripts

--- scripts/make.jocl.all.linux-x86.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if [ -e ../setenv-build-jogl-x86.sh ] ; then
-    . ../setenv-build-jogl-x86.sh
-fi
-
-ant  \
-    -Drootrel.build=build-x86 \
-    $* 2>&1 | tee make.jocl.all.linux-x86.log
--- scripts/make.jocl.all.linux-x86_64.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if [ -e ../setenv-build-jogl-x86_64.sh ] ; then
-    . ../setenv-build-jogl-x86_64.sh
-fi
-
-ant  \
-    -Drootrel.build=build-x86_64 \
-    $* 2>&1 | tee make.jocl.all.linux-x86_64.log
